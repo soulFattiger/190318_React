9a9a4a92fde622fd50a2ad5416f5e8dca5d1c95b
soul <soul@qq.com>
2019-07-23 23:33:53 +0800

    bbbbbb

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-/*
+/* 
 入口js
- */
+*/
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import 'antd/dist/antd.css'
 
 import App from './App'
-import storageUtils from './utils/storageUtils'
-import memoryUtils from './utils/memoryUtils'
+import store from './redux/store'
 
-// 读取local中保存user, 保存到内存中
-const user = storageUtils.getUser()
-memoryUtils.user = user
+ReactDOM.render(<App store={store}/>, document.getElementById('root'))
 
-
-// 将App组件标签渲染到index页面的div上
-ReactDOM.render(<App />, document.getElementById('root'))
+// 绑定监视store内部状态数据改变的监听
+store.subscribe(() => { // 重新渲染标签
+  ReactDOM.render(<App store={store}/>, document.getElementById('root'))
+})
